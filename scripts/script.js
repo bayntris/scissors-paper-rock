9d1b28e19976cd62858a1d9fcc9609ae9e358964
bayntris <tristanragtdotl@gmail.com>
2023-05-17 12:21:33 +1000

    Add Player Selection function

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,29 @@ function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
 
     if (number === 0) {
-        return 'Paper';
+        return 'paper';
     }
     else if (number === 1) {
-        return 'Scissors';
+        return 'scissors';
     }
     else if (number === 2) {
-        return 'Rock';
+        return 'rock';
     }
     else {
         return 'Sorry, something has gone wrong';
     }
+}
+
+function playerSelection() {
+    
+    let playerChoice = prompt('Please choose between Scissors, Paper or Rock', '').toLowerCase();
+    
+    if (playerChoice === 'paper' || playerChoice === 'scissors' || playerChoice === 'rock') {
+        return playerChoice;
+    }
+    else {
+        playerChoice = prompt('Invalid answer, please choose between Paper, Scissors or Rock', '');
+        return playerChoice.toLowerCase();
+    }
+    
 }
