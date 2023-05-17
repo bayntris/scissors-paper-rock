abd567d1ee5c89e627cb3554dcd380b1cc1a2bf4
bayntris <tristanragtdotl@gmail.com>
2023-05-17 12:46:32 +1000

    Complete playgame function

--- scripts/script.js
+++ scripts/script.js
@@ -20,11 +20,50 @@ function getPlayerSelection() {
     let playerSelection = prompt('Please choose between Scissors, Paper or Rock', '').toLowerCase();
     
     if (playerSelection === 'paper' || playerSelection === 'scissors' || playerSelection === 'rock') {
-        return playerChoice;
+        return playerSelection;
     }
     else {
         playerSelection = prompt('Invalid answer, please choose between Paper, Scissors or Rock', '');
         return playerSelection.toLowerCase();
     }
-    
 }
+
+    function singleRound(playerSelection, computerSelection) {
+        if (playerSelection === 'paper') {
+            if (computerSelection === 'scissors') {
+                return 'Computer Wins! Scissors beats Paper!';
+            }
+            else if (computerSelection === 'rock') {
+                return 'You Win! Paper beats Rock!';
+            }
+            else {
+                return 'Tie! Both chose Paper!';
+            }
+        }
+        else if (playerSelection === 'scissors') {
+            if (computerSelection === 'rock') {
+                return 'Computer Wins! Rock beats Scissors!';
+            }
+            else if (computerSelection === 'paper') {
+                return 'You Win! Scissors beats Paper!';
+            }
+            else {
+                return 'Tie! Both chose Scissors!';
+            }
+        }
+        else if (playerSelection === 'rock') {
+            if (computerSelection === 'paper') {
+                return 'Computer Wins! Paper beats Rock!';
+            }
+            else if (computerSelection === 'scissors') {
+                return 'You Win! Rock beats Scissors!';
+            }
+            else {
+                return 'Tie! Both chose Rock!';
+            }
+        }
+     
+    }
+
+    console.log(singleRound(getPlayerSelection(), getComputerSelection()));
+    
